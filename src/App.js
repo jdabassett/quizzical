a3dcfa428b8ed2d3d292204bd1b7b2083b02ee98
jdabassett <jacobbassett@gmail.com>
2023-01-27 21:40:14 -0700

    made static and undynamic version

--- src/App.js
+++ src/App.js
@@ -4,7 +4,76 @@ import './App.css';
 export default function App() {
   return (
     <div className="app-container">
-      <p>boogers</p>
+      <div className="landing-container">
+        <h1 className="landing-title text">Quizzical</h1>
+        <p className="landing-description text">Test your knowledge! Anwser these random assortment of questions from our friends at 'Open Trivia Database'. Do it to impress your friends, yourself, and most of all to gain the respect of your cat!</p>
+        <button className="landing-button button-submit">Start Quiz</button>
+      </div>
+      
+      <div className="quiz-container hidden">
+        <div className="quiz-question-container">
+          <p className="quiz-question">How would one say goodbye in Spanish?</p>
+          <div className="quiz-answers-container">
+            <button className="quiz-answer">Answer1</button>
+            <button className="quiz-answer">Answer2</button>
+            <button className="quiz-answer">Answer3</button>
+            <button className="quiz-answer">Long ass answer</button>
+          </div>
+          <hr></hr>
+        </div>
+
+        <div className="quiz-question-container">
+          <p className="quiz-question">How would one say goodbye in Spanish?</p>
+          <div className="quiz-answers-container">
+            <button className="quiz-answer">Answer1</button>
+            <button className="quiz-answer">Answer2</button>
+            <button className="quiz-answer">Answer3</button>
+            <button className="quiz-answer">Even Longer Ass Answer than that</button>
+          </div>
+          <hr></hr>
+        </div>
+
+        <div className="quiz-question-container">
+          <p className="quiz-question">How would one say goodbye in Spanish?</p>
+          <div className="quiz-answers-container">
+            <button className="quiz-answer">Answer1</button>
+            <button className="quiz-answer">Answer2</button>
+            <button className="quiz-answer">Answer3</button>
+            <button className="quiz-answer">This is the longes answer I dare write out loud</button>
+          </div>
+          <hr></hr>
+        </div>
+
+        <div className="quiz-question-container">
+          <p className="quiz-question">How would one say goodbye in Spanish?</p>
+          <div className="quiz-answers-container">
+            <button className="quiz-answer">Answer1</button>
+            <button className="quiz-answer">Answer2</button>
+            <button className="quiz-answer">Answer3</button>
+            <button className="quiz-answer">LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</button>
+          </div>
+          <hr></hr>
+        </div>
+
+        <div className="quiz-question-container">
+          <p className="quiz-question">How would one say goodbye in Spanish?</p>
+          <div className="quiz-answers-container">
+            <button className="quiz-answer">Answer1</button>
+            <button className="quiz-answer">Answer2</button>
+            <button className="quiz-answer">Answer3</button>
+            <button className="quiz-answer">Answer4</button>
+          </div>
+          <hr></hr>
+        </div>
+
+        <div className="quiz-results-submit-container">
+          <p className="quiz-results">Your scored 0/5 correctly!</p>
+          <button className="quiz-submit button-submit">Submit Answers</button>
+        </div>
+        
+
+      </div>
+      
     </div>
   );
 }
